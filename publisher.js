8adfc494e1ede041c2d46fb755ea0718548d2c31
eceyenmez <eceyenmez@gmail.com>
2021-02-19 17:09:40 +0300

    iterate over logs

--- publisher.js
+++ publisher.js
@@ -11,17 +11,16 @@ const pubSubClient = new PubSub();
 
 async function publishMessage(body) {
   // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
-  const data = JSON.stringify(body);
-  console.log("debug");
-  console.log(body);
-  console.log("end");
-  console.log(data);
-
-  const dataBuffer = Buffer.from(data);
 
   try {
-    const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
-    console.log(`Message ${messageId} published.`);
+
+  	for(const log in body){
+  		const data = JSON.stringify(body);
+        const dataBuffer = Buffer.from(data);
+        const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
+        console.log(`Message ${messageId} published.`);
+  	}
+    
   } catch (error) {
     console.error(`Received error while publishing: ${error.message}`);
     process.exitCode = 1;
