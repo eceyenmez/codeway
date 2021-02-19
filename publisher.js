94347d450188ee14f3941f5adf0a53d21b26a1bd
eceyenmez <eceyenmez@gmail.com>
2021-02-19 18:18:12 +0300

    fix

--- publisher.js
+++ publisher.js
@@ -15,7 +15,9 @@ async function publishMessage(body) {
   try {
 
   	for(const log in body){
+  		console.log(body[log]);
   		const data = JSON.stringify(log);
+  		console.log(data);
         const dataBuffer = Buffer.from(data);
         const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
         console.log(`Message ${messageId} published.`);
